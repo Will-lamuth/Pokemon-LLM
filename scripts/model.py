71e6049d6868d1f69dc0472d94b47ebd131ae80b
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2025-04-17 13:59:10 -0400

    ollama implementation
    -testing ollama with llama2

--- scripts/model.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained(
-    "meta-llama/Meta-Llama-3-8B-Instruct",
-    device_map="auto",  # uses MPS/CPU locally or GPU on your training machine
-    torch_dtype="auto"
-)
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
